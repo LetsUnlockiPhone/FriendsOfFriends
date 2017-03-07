c84b84b58872ae772caa84d8b7a42208b7b17c54
Noah <manwhoami@users.noreply.github.com>
2017-03-06 20:29:22 -0500

    Better error handling, PEP 8 updates
    Better error handling and PEP 8 styling convention adherence.

--- Friends of Friends.py
+++ Friends of Friends.py
@@ -1,10 +1,10 @@
 import base64, urllib2, json, datetime, time, sys, getpass, plistlib
-def tokenFactory(dsid, mmeAuthToken):
-    mmeAuthTokenEncoded = base64.b64encode("%s:%s" % (dsid, mmeAuthToken))
+def token_factory(dsid, mmeAuthToken):
+    mme_auth_token_encoded = base64.b64encode('%s:%s' % (dsid, mmeAuthToken))
     #now that we have proper auth code, we will attempt to get all account tokens.
-    url = "https://setup.icloud.com/setup/get_account_settings"
+    url = 'https://setup.icloud.com/setup/get_account_settings'
     headers = {
-        'Authorization': 'Basic %s' % mmeAuthTokenEncoded,
+        'Authorization': 'Basic %s' % mme_auth_token_encoded,
         'Content-Type': 'application/xml',
         'X-MMe-Client-Info': '<iPhone6,1> <iPhone OS;9.3.2;13F69> <com.apple.AppleAccount/1.0 (com.apple.Preferences/1.0)>'
     }
@@ -15,18 +15,18 @@ def tokenFactory(dsid, mmeAuthToken):
         response = urllib2.urlopen(request)
     except urllib2.HTTPError as e:
         if e.code != 200:
-            return ("HTTP Error: %s" % e.code, 0) #expects tuple
+            return ('HTTP Error: %s' % e.code, 0) #expects tuple
         else:
             print e
-            raise HTTPError
-    #staple it together & call it bad weather; only need FMFAppToken
+            raise urllib2.HTTPError
+    #only need FMFAppToken from here on out
     content = response.read()
-    mmeFMFAppToken = plistlib.readPlistFromString(content)["tokens"]["mmeFMFAppToken"]
-    return mmeFMFAppToken
+    mme_FMF_app_token = plistlib.readPlistFromString(content)['tokens']['mmeFMFAppToken']
+    return mme_FMF_app_token
 
-def dsidFactory(uname, passwd): #can also be a regular DSID with AuthToken
-    creds = base64.b64encode("%s:%s" % (uname, passwd))
-    url = "https://setup.icloud.com/setup/authenticate/%s" % uname
+def dsid_factory(uname, passwd): #can also be a regular DSID with AuthToken
+    creds = base64.b64encode('%s:%s' % (uname, passwd))
+    url = 'https://setup.icloud.com/setup/authenticate/%s' % uname
     headers = {
         'Authorization': 'Basic %s' % creds,
         'Content-Type': 'application/xml',
@@ -39,113 +39,118 @@ def dsidFactory(uname, passwd): #can also be a regular DSID with AuthToken
     except urllib2.HTTPError as e:
         if e.code != 200:
             if e.code == 401:
-                return ("HTTP Error 401: Unauthorized. Are you sure the credentials are correct?", 0)
+                return ('HTTP Error 401: Unauthorized. Are you sure the credentials are correct?', 0)
             elif e.code == 409:
-                return ("HTTP Error 409: Conflict. 2 Factor Authentication appears to be enabled. You cannot use this script unless you get your MMeAuthToken manually (generated either on your PC/Mac or on your iOS device).", 0)
+                return ('HTTP Error 409: Conflict. 2 Factor Authentication appears to be enabled. You cannot use this script unless you get your MMeAuthToken manually (generated either on your PC/Mac or on your iOS device).', 0)
             elif e.code == 404:
-                return ("HTTP Error 404: URL not found. Did you enter a username?", 0)
+                return ('HTTP Error 404: URL not found. Did you enter a username?', 0)
             else:
-                return ("HTTP Error %s.\n" % e.code, 0)
+                return ('HTTP Error %s.\n' % e.code, 0)
         else:
             print e
-            raise HTTPError
+            raise urllib2.HTTPError
     content = response.read()
-    DSID = int(plistlib.readPlistFromString(content)["appleAccountInfo"]["dsPrsID"]) #stitch our own auth DSID
-    mmeAuthToken = plistlib.readPlistFromString(content)["tokens"]["mmeAuthToken"] #stitch with token
-    return (DSID, mmeAuthToken)
+    DSID = int(plistlib.readPlistFromString(content)['appleAccountInfo']['dsPrsID']) #stitch our own auth DSID
+    mme_auth_token = plistlib.readPlistFromString(content)['tokens']['mmeAuthToken'] #stitch with token
+    return (DSID, mme_auth_token)
 
-def HeardItFromAFriendWho(dsid, mmeFMFAppToken, user):
+def heard_it_from_a_friend_who(dsid, mme_FMF_app_token, user):
     while 1:
         url = 'https://p04-fmfmobile.icloud.com/fmipservice/friends/%s/refreshClient' % dsid
         headers = {
-            'Authorization': 'Basic %s' % base64.b64encode("%s:%s" % (dsid, mmeFMFAppToken)),#FMF APP TOKEN
+            'Authorization': 'Basic %s' % base64.b64encode('%s:%s' % (dsid, mme_FMF_app_token)),#FMF APP TOKEN
             'Content-Type': 'application/json; charset=utf-8',
         }
         data = {
-            "clientContext": {
-                "appVersion": "5.0" #critical for getting appropriate config / time apparently.
+            'clientContext': {
+                'appVersion': '5.0' #critical for getting appropriate config / time apparently.
             }
         }
-        jsonData = json.dumps(data)
-        request = urllib2.Request(url, jsonData, headers)
-        i = 0
+        json_data = json.dumps(data)
+        request = urllib2.Request(url, json_data, headers)
         while 1:
             try:
                 response = urllib2.urlopen(request)
                 break
             except: #for some reason this exception needs to be caught a bunch of times before the request is made.
-                i +=1
                 continue
-        x = json.loads(response.read())
-        dsidList = []
-        phoneList = [] #need to find how to get corresponding name from CalDav
-        for y in x["following"]: #we need to get contact information.
-            for z, v in y.items():
+        response_dict = json.loads(response.read())
+        dsid_list = []
+        phone_list = [] #need to find how to get corresponding name from CalDav
+        for friend in response_dict['following']: #we need to get contact information.
+            for key, value in friend.items():
                 #do some cleanup
-                if z == "invitationAcceptedHandles":
-                    v = v[0] #v is a list of contact information, we will grab just the first identifier
-                    phoneList.append(v)
-                if z == "id":
-                    v = v.replace("~", "=")
-                    v = base64.b64decode(v)
-                    dsidList.append(v)
-        zippedList = zip(dsidList, phoneList)
-        retString = ""
-        i = 0
-        for y in x["locations"]:#[0]["location"]["address"]:
-            streetAddress, country, state, town, timeStamp = " " *5
-            dsid = y["id"].replace("~", "=")
+                if key == 'invitationAcceptedHandles':
+                    value = value[0] #value is a list of contact information, we will grab just the first identifier
+                    phone_list.append(value)
+                if key == 'id':
+                    value = value.replace('~', '=')
+                    value = base64.b64decode(value)
+                    dsid_list.append(value)
+        zipped_list = zip(dsid_list, phone_list)
+        return_string = ''
+        user_count = 0
+        try:
+            locations = response_dict['locations']
+        except KeyError:
+            appendable = ''
+            if dsid_list:
+                appendable = ' for the following users %s ' % dsid_list
+            return 'Error getting locations%s.' % appendable
+        for friend in response_dict['locations']:
+            street_address, country, state, town, timeStamp = ' ' *5
+            dsid = friend['id'].replace('~', '=')
             dsid = base64.b64decode(dsid) #decode the base64 id, and find its corresponding one in the zippedList.
-            for g in zippedList:
-                if g[0] == dsid:
-                    phoneNumber = g[1] #we should get this for every person. no errors if no phone number found. 
+            for user_id in zipped_list:
+                if user_id[0] == dsid:
+                    phone_number = user_id[1] #we should get this for every person. no errors if no phone number found. 
             
             try:
-                timeStamp = y["location"]["timestamp"] / 1000
-                timeNow = time.time()
-                timeDelta = timeNow - timeStamp #time difference in seconds
-                minutes, seconds = divmod(timeDelta, 60) #great function, saves annoying maths
+                time_stamp = friend['location']['timestamp'] / 1000
+                time_now = time.time()
+                time_delta = time_now - time_stamp #time difference in seconds
+                minutes, seconds = divmod(time_delta, 60) #great function, saves annoying maths
                 hours, minutes = divmod(minutes, 60)
-                timeStamp = datetime.datetime.fromtimestamp(timeStamp).strftime("%A, %B %d at %I:%M:%S")
-                timeStamp = "%s (%sm %ss ago)" % (timeStamp, str(minutes).split(".")[0], str(seconds).split(".")[0]) #split at decimal
+                time_stamp = datetime.datetime.fromtimestamp(time_stamp).strftime('%A, %B %d at %I:%M:%S')
+                time_stamp = '%s (%sm %ss ago)' % (time_stamp, str(minutes).split('.')[0], str(seconds).split('.')[0]) #split at decimal
             except TypeError:
-                timeStamp = "Could not get last location time."
+                time_stamp = 'Could not get last location time.'
 
-            if not y["location"]: #once satisfied, all is good, return fxn will end
+            if not friend['location']: #once satisfied, all is good, return fxn will end
                 continue #go back to top of loop and re-run query
             
-            for z, v in y["location"]["address"].items(): #loop through address info
+            for key, value in friend['location']['address'].items(): #loop through address info
                 #counter of threes for pretty print...
-                if type(v) is list:
+                if type(value) is list:
                     continue
-                if z == "streetAddress":
-                    streetAddress = v
-                if z == "countryCode":
-                    country = v
-                if z == "stateCode":
-                    state = v
-                if z == "locality":
-                    town = v
+                if key == 'streetAddress':
+                    street_address = value
+                if key == 'countryCode':
+                    country = value
+                if key == 'stateCode':
+                    state = value
+                if key == 'locality':
+                    town = value
 
-            if streetAddress != " ": #in the event that we cant get a street address, dont print it to the final thing
-                retString += "%s\n%s\n%s, %s, %s\n%s\n%s\n" % ("\033[34m" + phoneNumber, "\033[92m" + streetAddress, town, state, country, "\033[0m" + timeStamp,"-----")
+            if street_address != ' ': #in the event that we cant get a street address, dont print it to the final thing
+                return_string += '%s\n%s\n%s, %s, %s\n%s\n%s\n' % ('\033[34m' + phone_number, '\033[92m' + street_address, town, state, country, '\033[0m' + time_stamp,'-----')
             else:
-                retString += "%s\n%s, %s, %s\n%s\n%s\n" % ("\033[34m" + phoneNumber, "\033[92m" + town, state, country, "\033[0m" + timeStamp,"-----")
+                return_string += '%s\n%s, %s, %s\n%s\n%s\n' % ('\033[34m' + phone_number, '\033[92m' + town, state, country, '\033[0m' + time_stamp,'-----')
 
-            i += 1
-        return retString + "\033[91mFound \033[93m[%s]\033[91m friends for %s!\033[0m" % (i, user)
+            user_count += 1
+        return '%s\033[91mFound \033[93m[%s]\033[91m friends for %s!\033[0m' % (return_string, user_count, user)
 
 if __name__ == '__main__':
-    user = raw_input("Apple ID: ")
+    user = raw_input('Apple ID: ')
     try: #in the event we are supplied with an DSID, convert it to an int
         int(user)
         user = int(user)
     except ValueError: #otherwise we have an apple id and can not convert
         pass
     passw = getpass.getpass()
-    (DSID, authToken) = dsidFactory(user, passw)
-    if authToken == 0: #http error
+    (DSID, auth_token) = dsid_factory(user, passw)
+    if auth_token == 0: #http error
         print DSID
         sys.exit()
-    mmeFMFAppToken = tokenFactory(DSID, authToken)
-    print HeardItFromAFriendWho(DSID, mmeFMFAppToken, user),
+    mme_FMF_app_token = token_factory(DSID, auth_token)
+    print heard_it_from_a_friend_who(DSID, mme_FMF_app_token, user),
